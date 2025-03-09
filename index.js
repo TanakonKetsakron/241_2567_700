9060b7739de3890c82bab64ff7cad24155d1bdb2
tanakon <your.email@example.com>
2025-03-09 12:53:10 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-function submitData(){ //index.js
-    let firstnameDOM = document.querySelector('input[name=firstname]');
-    let lastnameDOM = document.querySelector('input[name=lastname]');
+const submitData = async () => {
+    let firstNameDOM = document.querySelector('input[name=firstname]');
+    let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]');
-    let interestDOM = document.querySelectorAll('input[name=interest]:checked');
+    let genderDOM = document.querySelector('input[name=gender]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
-    
+
+    let messageDOM = document.getElementById('message');
+    try {
+
     let interest = ''
-    for(let i = 0; i < interestDOM.length; i++){
-        interest += interestDOM[i].value 
-        if(i != interestDOM.length - 1){
-            interest += ','
-        }
+    for (let i = 0; i < interestDOMs.length; i++) {
+              interest += interestDOMs[i].value
+              if (i != interestDOMs.length - 1) {
+                        interest += ', '
+              }
     }
-    let userData ={
-        firstName: firstnameDOM.value,
-        lastName: lastnameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
-        
+
+    let userData = {
+              firstName: firstNameDOM.value,
+              lastName: lastNameDOM.value,
+              age: ageDOM.value,
+              gender: genderDOM.value,
+              description: descriptionDOM.value,
+              interests: interest
+
     }
-    console.log('submitData',userData);
+    console.log('submitData', userData);
+    
+      const response = await axios.post('http://localhost:8000/users', userData)
+      console.log('response', response.data);
+      messageDOM.innerText = 'บันทึกข้อมูลเรียบร้อยแล้ว'
+      messageDOM.className ='message success'
+    }catch(err) {
+      if (err.response) {
+        console.log("err.response",err.response.data.message);
+      }
+      messageDOM.innerText = 'บันทึกข้อมูลไม่สำเร็จ'
+      messageDOM.className ='message danger'
+    }
+   
 }
--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const mysql = require('mysql2/promise');
 const cors = require('cors');
 const app = express();
 
-const port = 8000;
+
+ const port = 8000;
  app.use(bodyParser.json());
  app.use(cors());
 
@@ -20,6 +21,30 @@ const initMySQL = async () => {
   
   })
 }
+
+const validateData = (userData) => {
+  let errors = [];
+  if (!userData.firstName === '') {
+      errors.push('กรุณากรอกชื่อ');
+  }
+  if (!userData.lastName === '') {
+      errors.push('กรุณากรอกนามสกุล');
+  }
+  if (!userData.age === '') {
+      errors.push('กรุณากรอกอายุ');
+  }
+  if (!userData.gender === '') {
+      errors.push('กรุณาเลือกเพศ');
+  }
+  if (!userData.interests === '') {
+      errors.push('กรุณาเลือกสิ่งที่สนใจ');
+  }
+  if (!userData.description === '') {
+      errors.push('กรุณากรอกคำอธิบาย');
+ return errors;
+}
+}
+
 app.get('/users',async (req, res) => {
   const results = await conn.query('SELECT * FROM users')
   res.json(results[0])
@@ -29,6 +54,15 @@ app.post('/users',async (req, res) => {
 
 try{
   let user = req.body;
+  const errors = ValidateData(user)
+  if(errors.length > 0){
+    throw { 
+      message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+      errors: errors
+    }
+  }
+
+
   const results= await conn.query('INSERT INTO users SET ?', user)
   res.json({
     message: 'Create user successfully',
